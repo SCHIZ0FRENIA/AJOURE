f19148a95d726608d4d1b9bf7bfcaa241fab2a22
voivod048 <maleyok2@gmail.com>
2023-07-05 14:29:39 +0300

    gdsh

--- back.js
+++ back.js
@@ -1,71 +1,47 @@
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
 import express from "express";
-import mongoose from 'mongoose'
 import cors from "cors";
+const MongoClient = require('mongodb').MongoClient;
 
 const app = express();
 
 const PORT = 7777;
-
-const UserModel = new mongoose.Schema({
-  email: {type: String, required: true},
-  phone: {type: String, required: true},
-  name: {type: String, required: true},
-  isCons: {type: Boolean, required: true},
-  answers: [
-    {type: Number, required: false}
-  ],
-});
-
-const Client = mongoose.model('clients', UserModel);
+const url = "mongodb://127.0.0.1:27017/";
+const mongoClient = new MongoClient(url);
 
 app.use(cors());
 app.use(express.json());
 
 app.post('/client/add', async (req, res) => {
-  await mongoose.connect('mongodb://localhost:27017/Ajoure')
-  .then(() => {
-    console.log('db is ok');
-  })
-  .catch(err => {
-    console.log('cant connect to db', err);
-  });
-  console.log(req?.body);
-
   try {
-    const {email, number, name, isCons} = req.body;
-
-    const doc = new Client({
-      email,
-      phone: number,
-      name, 
-      isCons,
-      answers: req?.body?.answers
-    });
-  
-    await doc.save();
-    await mongoose.disconnect();
-  } catch(err) {
-    console.log(err.message);
+    let document = req.body;
+    await mongoClient.connect();
+    const db = mongoClient.db("Ajoure");
+    const collection = db.collection("clients");
+    const result = await collection.insertOne(document);
+    console.log(result);
+    console.log(document);
+  }catch(err) {
+    console.log(err);
+  } finally {
+    await mongoClient.close();
   }
-
   return res.json({message: "sucsess"});
 });
 
 app.get("/getusers", async (req, res) => {
-  await mongoose.connect('mongodb://localhost:27017/Ajoure');
-  const users = await Client.find({});
+  await mongoClient.connect();
+  const db = mongoClient.db("Ajoure");
+  const collection = db.collection("clients");
+  const users = await collection.find().toArray();
+  console.log(users);
   res.send(users);
-  
 });
-
-app.post('/deleteuser', async (req, res) => {
-  
-})
 
 app.listen(PORT, err => {
   if(err) {
     return console.log(err);
   }
-
   console.log('server is ok');
 });
